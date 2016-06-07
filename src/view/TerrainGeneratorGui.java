bb10a1b7d8ad6e01bde1a0159d81d9593f432004
nhochberger <nico@irrwisch.net>
2016-06-07 14:54:33 +0200

    Changes due to changes in Utilities

--- src/view/TerrainGeneratorGui.java
+++ src/view/TerrainGeneratorGui.java
@@ -1,15 +1,12 @@
 package view;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 import controller.events.TerrainGeneratedEvent;
 import controller.events.TerrainGenerationProgressEvent;
-import hochberger.utilities.application.ApplicationShutdownEvent;
 import hochberger.utilities.application.session.BasicSession;
 import hochberger.utilities.application.session.SessionBasedObject;
 import hochberger.utilities.eventbus.EventReceiver;
 import hochberger.utilities.gui.ApplicationGui;
+import hochberger.utilities.gui.WindowClosedApplicationShutdownEventPublisher;
 
 public class TerrainGeneratorGui extends SessionBasedObject implements ApplicationGui {
 
@@ -23,13 +20,7 @@ public class TerrainGeneratorGui extends SessionBasedObject implements Applicati
     public void activate() {
         this.mainFrame = new TerrainGeneratorMainFrame(session());
         this.mainFrame.show();
-        this.mainFrame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosed(final WindowEvent arg0) {
-                super.windowClosed(arg0);
-                session().getEventBus().publishFromEDT(new ApplicationShutdownEvent());
-            }
-        });
+        this.mainFrame.addWindowListener(new WindowClosedApplicationShutdownEventPublisher(session()));
         session().getEventBus().register(new TerrainGenerationProgressForwarder(), TerrainGenerationProgressEvent.class);
         session().getEventBus().register(new TerrainGeneratedEventForwarder(), TerrainGeneratedEvent.class);
     }
