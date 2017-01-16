8247141d34d3cb07919082186e98ce3dc6443420
nhochberger <nico@irrwisch.net>
2017-01-16 19:26:38 +0100

    workson #26
    adjustment to average height

--- src/model/NaturalSurfaceDiamondSquareGenerator.java
+++ src/model/NaturalSurfaceDiamondSquareGenerator.java
@@ -16,9 +16,29 @@ public class NaturalSurfaceDiamondSquareGenerator extends DiamondSquareGenerator
     @Override
     public SurfaceMap generate(final int dimension, final double roughness, final double elevation, final int erosion, final double boulderAmountFactor) {
         final SurfaceMap surface = super.generate(dimension, roughness, elevation, erosion, boulderAmountFactor);
+        adjustSubAverageElevation(surface);
         logger().info("Fractal base surface generated");
         this.eroder.erode(surface, erosion);
         this.obstacleGenerator.generateAndDistributeObstacles(surface, boulderAmountFactor);
         return surface;
+    }
+
+    private void adjustSubAverageElevation(final SurfaceMap surface) {
+        double sum = 0;
+        for (int z = 0; z < surface.getZDimension(); z++) {
+            for (int x = 0; x < surface.getXDimension(); x++) {
+                sum += surface.get(x, z);
+            }
+        }
+        final double average = sum / (surface.getXDimension() * surface.getZDimension());
+        for (int z = 0; z < surface.getZDimension(); z++) {
+            for (int x = 0; x < surface.getXDimension(); x++) {
+                final double y = surface.get(x, z);
+                if (average > y) {
+                    final double newY = ((y - average) / 3) + average;
+                    surface.set(x, z, newY);
+                }
+            }
+        }
     }
 }
